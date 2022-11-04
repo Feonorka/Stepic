6b817747956ec91210878b2f38e8e3b84d12a364
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-11-04 23:36:51 +0300

    Initial commit

---
+++ Exp/Task_0.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+print(end="")
+for k in range(c, d + 1):
+    print("\t", k, end="")
+print()
+for i in range(a, b+1):
+    print(i, "", end="")
+    for j in range(c, d+1):
+        print("\t", end="")
+        print(i * j, end="")
+    print()
