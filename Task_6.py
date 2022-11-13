47d878b407767f5634c318fb06334f6c3d6658aa
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-11-14 00:19:27 +0300

    Попытка

--- Task_6.py
+++ Task_6.py
@@ -1,3 +1,8 @@
 spisok = [int(i) for i in input().split()]
+list = []
 spisok.sort()
-print(spisok)
+for i in range(len(spisok)):
+    if spisok.count(spisok[i]) > 1:
+        list.append(spisok[i])
+        if spisok[i] in list:
+            print(list[i], end=" ")
