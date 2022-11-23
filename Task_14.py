0750c72b3351959d849f4b8b2aec80643071c54f
Feonorka <104940479+Feonorka@users.noreply.github.com>
2022-11-23 23:37:05 +0300

    Добавлено решеине

--- Task_14.py
+++ Task_14.py
@@ -1,6 +1,8 @@
-with open('Exp_2\dataset_3363_2.txt') as file_in:
+with open('dataset_3363_2.txt') as file_in:
     for line in file_in:
         line = line.strip()
+
+#line = 'S4B2h4v5n2o1P11S13'
 
 my_list = list(line) # Конвертация строки в список
 num_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -8,6 +10,7 @@ my_dict = dict()
 temp_list = []
 i = 0
 k= 0
+end_str = ''
 for item in range(len(my_list)):
     if my_list[i] in num_list:
         if my_list[i - 1] not in num_list: 
@@ -21,6 +24,7 @@ for item in range(len(my_list)):
                     break
             temp_str = "".join(temp_list)
             my_dict[my_list[i - 1]] = int(temp_str)
+            end_str = end_str + my_list[i -1] * int(temp_str)
             i += 1
             temp_list.clear()
         else:
@@ -31,7 +35,7 @@ new_list = [key * val for key, val in my_dict.items()]
 k = 0
 for k in range(len(new_list)):
     print(new_list[k], end="")
-
-final_str = "".join(new_list)
+print()
+print(end_str)
 with open("out.txt", 'w') as file_out:
-    file_out.write(final_str)
+    file_out.write(end_str)
